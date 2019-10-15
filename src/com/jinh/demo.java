c7a7f11c9864d84de2b8f107232e327e11d9a29d
jinh-git <1292440426@qq.com>
2019-10-15 15:13:37 +0800

    第一次提交

---
+++ src/com/jinh/demo.java
@@ -0,0 +1,26 @@
+package com.jinh;
+
+import java.util.ArrayList;
+
+public class demo {
+    public static final int a=10;
+    private static final int b=20;
+    public static void main(String[] args) {
+
+        System.out.println("aa");
+        ArrayList arrayList = new ArrayList();
+        if (arrayList == null) {
+
+        }
+        if (arrayList != null) {
+
+        }
+        for (int i = 0; i <1; i++) {
+
+        }
+        for (String arg : args) {
+
+        }
+
+    }
+}
---
+++ src/com/jinh/demo2.java
@@ -0,0 +1,16 @@
+package com.jinh;
+
+import java.util.HashMap;
+
+/**
+ * @author jinh
+ * @date 2019-10-14 22:51
+ */
+public class demo2 {
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("li","li");
+        hashMap.remove("li");
+        hashMap.clear();
+    }
+}
